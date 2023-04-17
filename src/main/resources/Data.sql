263bdf5e9e8dfcf5d7f73f79101fa88ca16a704c
Bril3d <khaledtbk@gmail.com>
2023-04-18 00:34:23 +0100

    Pre-final Version

---
+++ src/main/resources/Data.sql
@@ -0,0 +1,8 @@
+insert into todo (ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(10001,'Briled','Get AWS Certified',CURRENT_DATE(),false);
+
+insert into todo (ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(10002,'Briled','Get DEVOPS CERTIFIED',CURRENT_DATE(),false);
+
+insert into todo (ID,USERNAME,DESCRIPTION,TARGET_DATE,DONE)
+values(10003,'Briled','Get AZURE Certified',CURRENT_DATE(),false);
